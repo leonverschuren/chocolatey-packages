3c5c5b7dbb402cafa1f4d3e07dfbabd37a9c4e65
Leon Verschuren <leon.verschuren@outlook.com>
2016-08-05 13:57:02 +0200

    GrabIt package

---
+++ packages/grabit/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'grabit'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://download.shemes.com/GrabIt173b.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  silentArgs   = '/VERYSILENT'
+
+  softwareName  = 'grabit*'
+  checksum      = 'a8fc2490262c49d033f786de59b98669'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
