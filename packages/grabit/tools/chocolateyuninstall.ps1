783ed10190e80575572d3c02b213e09bac6bf15e
Leon Verschuren <leon.verschuren@outlook.com>
2016-08-05 14:36:19 +0200

    Fixed GrabIt uninstall script

--- packages/grabit/tools/chocolateyuninstall.ps1
+++ packages/grabit/tools/chocolateyuninstall.ps1
@@ -3,32 +3,20 @@
 $packageName = 'grabit'
 $softwareName = 'grabit*'
 $installerType = 'EXE' 
+$silentArgs = '/VERYSILENT'
+$validExitCodes = @(0)
 
-$silentArgs = '/qn /norestart'
-$validExitCodes = @(0, 3010, 1605, 1614, 1641)
-if ($installerType -ne 'MSI') {
-  $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
-  $validExitCodes = @(0)
-}
-
-$uninstalled = $false
 [array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
 
 if ($key.Count -eq 1) {
-  $key | % { 
-    $file = "$($_.UninstallString)"
-
-    if ($installerType -eq 'MSI') {
-      $silentArgs = "$($_.PSChildName) $silentArgs"
-
-      $file = ''
-    }
+  $key | % {
+    $file = $($_.UninstallString)
 
     Uninstall-ChocolateyPackage -PackageName $packageName `
                                 -FileType $installerType `
                                 -SilentArgs "$silentArgs" `
                                 -ValidExitCodes $validExitCodes `
-                                -File "$file"
+                                -File $($file.Replace('"',''))
   }
 } elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
